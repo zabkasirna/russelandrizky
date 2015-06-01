2c305e10aa5e8d05d4c3308d272604a73f92353d
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-06-01 20:15:06 +0700

    Temp Stop.

--- src/template/page.php
+++ src/template/page.php
@@ -12,7 +12,6 @@ get_header(); ?>
             <main id="main" role="main">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-                <h1 class="h2 uc"><?php the_title();?></h1>
                 <?php the_content();?>
 
             <?php // End the loop. ?>
