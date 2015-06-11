9a3952e95f068f807367a0150e3f6ed5c6128cf8
Zabka Sirna Gwen <zabka.sirna.gwen@gmail.com>
2015-06-12 06:45:25 +0700

    Successfully overriden Default Color by Custom Color.

--- src/template/header.php
+++ src/template/header.php
@@ -31,7 +31,90 @@
     <?php // end analytics ?>
 </head>
 
-<body <?php body_class( 'is-preload' ); ?>>
+<?php
+    global $wp_query;
+    $post_id = $wp_query -> post -> ID;
+?>
+
+<?php
+
+    // Body css class modifiers
+
+    $body_class = array();
+
+    // Add Debuggrr toggler to body class
+    if( get_field( 'enable_debuggrr', 'options' ) ) :
+        $is_debugged = get_field( 'enable_debuggrr', 'options' );
+
+        if ( $is_debugged ) :
+            $body_class[] = 'is-debugged';
+        endif;
+
+    endif;
+
+    // Add preload toggler to body class
+    $body_class[] = 'is-preload';
+
+?>
+
+<?php
+    
+    // Body Color
+    $body_data_color = array();
+
+    // Get Default Color
+    if ( have_rows( 'default_colors', 'options' ) ) : while ( have_rows( 'default_colors', 'options' ) ) :
+
+            the_row();
+
+            // String for body inline-style
+            $body_default_bgc = get_sub_field( 'colors_default_bg' );
+
+            // String for body data-attr
+            $body_default_data_color = array(
+                "fg" => get_sub_field( 'colors_default_fg' ),
+                "bg" => get_sub_field( 'colors_default_bg' ),
+            );
+
+    endwhile; endif;
+
+    // Get Custom Color
+    if ( have_rows( 'custom_colors', $post_id ) ) : while ( have_rows( 'custom_colors', $post_id ) ) :
+
+            the_row();
+
+            // String for body inline-style
+            $body_custom_bgc = get_sub_field( 'colors_custom_bg' );
+
+            // String for body data-attr
+            $body_custom_data_color = array(
+                "fg" => get_sub_field( 'colors_custom_fg' ),
+                "bg" => get_sub_field( 'colors_custom_bg' ),
+            );
+
+    endwhile; endif;
+
+    // Apply value if only default value is overidden by custom value
+    $body_bgc = 'background-color: ' . ( $body_custom_bgc ? $body_custom_bgc : $body_default_bgc ) . '; ';
+    $body_data_color = array(
+        "fg" => ( $body_custom_data_color['fg'] ? $body_custom_data_color['fg'] : $body_default_data_color['fg'] ),
+        "bg" => ( $body_custom_data_color['bg'] ? $body_custom_data_color['bg'] : $body_default_data_color['bg'] )
+    );
+?>
+
+<body
+    <?php body_class( $body_class ); ?>
+
+    <?php
+        echo ' style="' . $body_bgc . '"';
+    ?>
+
+    data-color='<?php echo json_encode( $body_data_color ); ?>'
+>
+
+<?php debuggrr( $post_id ); ?>
+<?php /*debuggrr( $body_class );*/ ?>
+<?php /*debuggrr( $body_data_color );*/ ?>
 
 <div id="page" class="hfeed site">
 
@@ -41,7 +124,7 @@
             
             <div id="header-brand">
                 <a href='/' rel="nofollow" class="logo-anchor">
-                    <object type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/uploads/images/logo/logo_head_default.svg' ?>" class="logo">RNR</object>
+                    <object class="logo-object" type="image/svg+xml" data="<?php echo get_template_directory_uri() . '/uploads/images/logo/logo_head_default.svg' ?>">RNR</object>
                 </a>
             </div>
 
